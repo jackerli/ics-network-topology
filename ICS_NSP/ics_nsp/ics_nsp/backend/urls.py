836b407d833d5a18c11715b4069da85118b6ef5b
libo <1481145273@qq.com>
2019-04-26 17:36:38 +0800

    删除ics_nsp

--- ICS_NSP/ics_nsp/ics_nsp/backend/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from topology import views
-urlpatterns = [
-    path('', views.saveTopology)
-]
--- ICS_NSP/ics_nsp/ics_nsp/usermanager/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    user_name = models.CharField(max_length=20)
-    user_passwd = models.CharField(max_length=20)
-    user_email = models.CharField(max_length=20)
